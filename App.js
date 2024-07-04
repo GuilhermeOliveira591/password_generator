f0fca26688c85c849b16e10d82a574247f23b414
Guilherme Gomes <guilhermegfoliveira2020@gmail.com>
2024-07-04 13:32:10 -0300

    Interface gráfica inicial criada

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Slider, {} from '@react-native-community/slider';
 
-export default function App() {
-  return (
+
+
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+       source={require("./src/assets/logo.png")}
+       style={styles.logo}
+      />
+
+      <Text style={styles.title}> 20 caracteres </Text>
+
+      <View style={styles.area}>
+        <Slider
+        style={{ height: 50 }}
+        minimumValue={6}
+        maximumValue={20}
+        maximumTrackTintColor='#FF0000'
+        minimumTrackTintColor='#000000'
+        thumbTintColor='#392DE9'
+        />
+
+      </View>
+      
+      <TouchableOpacity style ={styles.button}>
+        <Text style={styles.buttonText}> Gerar Senha </Text>
+      </TouchableOpacity>
+
+
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#F3F3FF",
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  logo:{
+    marginBottom: 60
+  },
+  area:{
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    padding: 6
+  },
+  button:{
+    backgroundColor: "#392DE9",
+    width: "80%",
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    marginBottom: 18
+  },
+  buttonText:{
+    color: "#FFFFFF",
+    fontSize: 20
+  }
+})
