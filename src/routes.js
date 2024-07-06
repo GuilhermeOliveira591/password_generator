fc1d881ca8c803f0620cd991a62395621117659e
Guilherme Gomes <guilhermegfoliveira2020@gmail.com>
2024-07-06 12:28:43 -0300

    App finalizado

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from './pages/home';
-import { Passwords } from '.pages/passwords';
+import { Passwords } from './pages/passwords';
 import { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
